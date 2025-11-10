993e370951ddc2edc079184f74021b4a325825df
Valentayne <yehoianvb0510@gmail.com>
2025-11-10 14:46:40 +0200

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by Валентин Єгоян on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
